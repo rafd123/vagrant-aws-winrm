c86c870d8836011583503a2e02a7bbe979df3a3b
Rafael Goodman <rafael@getthefrog.com>
2015-11-13 18:07:56 -0500

    Add logging

--- lib/vagrant-aws-winrm/capability.rb
+++ lib/vagrant-aws-winrm/capability.rb
@@ -1,3 +1,4 @@
+require "log4r"
 require "aws-sdk"
 
 module VagrantPlugins
@@ -5,8 +6,10 @@ module VagrantPlugins
     module WinRM
       class Capability
         def self.winrm_info(machine)
+          logger = Log4r::Logger.new("vagrant_aws_winrm::capability::winrm_info")
+
           if machine.config.winrm.password == :aws
-            machine.ui.info('Getting WinRM password from AWS')
+            machine.ui.info('Getting WinRM password from AWS...')
 
             # AWS connection info
             access_key_id     = machine.provider_config.access_key_id
@@ -15,15 +18,20 @@ module VagrantPlugins
             region            = machine.provider_config.region
             region_config     = machine.provider_config.get_region_config(region)
             endpoint          = region_config.endpoint                        
-            options           = {
-                                  region:          region,
-                                  credentials:     credentials
-                              }
+
+            options = {
+              region:       region,
+              credentials:  credentials
+            }
 
             # Account for custom endpoints (e.g. OpenStack)   
-            options[:endpoint] = endpoint if endpoint
+            options[:endpoint] = endpoint if endpoint      
 
-            # Fetch that password data for the instance
+            logger.info("Getting password data from AWS...")
+            logger.info(" -- Region: #{region}")
+            logger.info(" -- Endpoint: #{endpoint}") if endpoint
+            logger.info(" -- Instance ID: #{machine.id}")
+
             ec2                 = Aws::EC2::Client.new(options)
             password_data       = ec2.get_password_data({ instance_id: machine.id }).password_data
             password_data_bytes = Base64.decode64(password_data)
@@ -31,11 +39,15 @@ module VagrantPlugins
             # Try to decrypt the password data using each one of the private key files
             # set by the user until we hit one that decrypts successfully
             machine.config.ssh.private_key_path.each do |private_key_path|
-              rsa = OpenSSL::PKey::RSA.new File.read(File.expand_path private_key_path)
+              private_key_path = File.expand_path private_key_path
 
+              logger.info("Decrypting password data using #{private_key_path}")
+              rsa = OpenSSL::PKey::RSA.new File.read private_key_path
               begin
                 machine.config.winrm.password = rsa.private_decrypt password_data_bytes
+                logger.info("Successfully decrypted password data using #{private_key_path}")
               rescue OpenSSL::PKey::RSAError
+                logger.warn("Failed to decrypt password data using #{private_key_path}")
                 next
               end
 
--- lib/vagrant-aws-winrm/plugin.rb
+++ lib/vagrant-aws-winrm/plugin.rb
@@ -14,9 +14,39 @@ module VagrantPlugins
         DESC
 
         provider_capability(:aws, :winrm_info) do
+          setup_logging
+          
           require_relative 'capability'
           VagrantPlugins::AWS::WinRM::Capability
         end
+
+        def self.setup_logging
+          require "log4r"
+
+          level = nil
+          begin
+            level = Log4r.const_get(ENV["VAGRANT_LOG"].upcase)
+          rescue NameError
+            # This means that the logging constant wasn't found,
+            # which is fine. We just keep `level` as `nil`. But
+            # we tell the user.
+            level = nil
+          end
+
+          # Some constants, such as "true" resolve to booleans, so the
+          # above error checking doesn't catch it. This will check to make
+          # sure that the log level is an integer, as Log4r requires.
+          level = nil if !level.is_a?(Integer)
+
+          # Set the logging level on all "vagrant" namespaced
+          # logs as long as we have a valid level.
+          if level
+            logger = Log4r::Logger.new("vagrant_aws_winrm")
+            logger.outputters = Log4r::Outputter.stderr
+            logger.level = level
+            logger = nil
+          end
+        end        
       end
     end    
   end
